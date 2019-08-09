11b0867fd9d7fcdd27818bc3829d5758e27b077c
vishal <vishalkale151071@gmail.com>
2019-08-09 15:56:42 +0530

    login form added in login app.

--- AttendanceIS/attendanceis/login/urls.py
+++ AttendanceIS/attendanceis/login/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('verify/', views.verify, name='verify'),
 ]
--- AttendanceIS/attendanceis/login/views.py
+++ AttendanceIS/attendanceis/login/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, HttpResponse
 from pymongo import MongoClient
-from .models import Teacher
+
+
 def login(request):
+
+    return render(request, 'login/login.html', {})
+
+
+def verify(request):
     client = MongoClient()
     db = client['attendance']
     collection = db['login_teacher']
-    data = collection.find_one()
-    l = []
-    for x in data:
-        l.append(x)
-    print(l)
-    for i in l:
-        print(data[i])
-    return HttpResponse('<h1>Login Page</h1>')
+    query = {'name':'{}'.format(request.POST.get('uname'))}
+    data = collection.find(query)
+
+    if data.count():
+        return HttpResponse('<h1>Valid</h1>')
+    else:
+        return HttpResponse('<h1>Invalid</h1>')
